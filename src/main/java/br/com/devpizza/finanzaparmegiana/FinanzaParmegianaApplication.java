fefd71cfd6dbf2ad37336d652e4f79f7425e596c
miguelschuldt <miguelizador@gmail.com>
2019-07-16 12:04:34 -0300

    idea confs

--- src/main/java/br/com/devpizza/finanzaparmegiana/FinanzaParmegianaApplication.java
+++ src/main/java/br/com/devpizza/finanzaparmegiana/FinanzaParmegianaApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanzaParmegianaApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(FinanzaParmegianaApplication.class, args);
-    }
+    public class FinanzaParmegianaApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(FinanzaParmegianaApplication.class, args);
+        }
 
 }
